aabd101f937d1bee3ab64626ed9d513a146f72a0
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-04-02 14:18:11 -0300

    update

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -20,4 +20,17 @@ class ProdutoController extends Controller
         });
         return response()->json($produtosComImagem);
     }
+
+    public function store (Request $request){
+        $produtoData = $request->all();
+
+        if($request->hasFile('imagem')){
+            $imagem = $request->file('imagem');
+            $nomeImagem = time().'.'.$imagem->getClientOriginalExtension();
+            $caminhoImagem = $imagem->storeAs('imagens/produtos', $nomeImagem, 'public');
+            $produtoData['imagem'] = $caminhoImagem;
+        }
+        $produto = Produto::create($produtoData);
+        return response()->json(['produto' => $produto], 201);
+    }
 }
